77ff623f6c66141ea23bd63656c33422ecb8895d
Samarjeet-001 <samarjeetd09@gmail.com>
2025-07-03 15:24:30 +0530

    Prime No. & Even Odd Code

---
+++ main.cpp
@@ -0,0 +1,48 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "Hello, World" << endl;
+//     return 0;
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter Number: ";
+//     cin >> n;
+//     if(n % 2 == 0)
+//     {
+//         cout << "Number is Even";
+//     }
+//     else
+//     {
+//         cout << "Number is odd";
+//     }
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     int n,count = 0;
+//     cout << "Enter Number: ";
+//     cin >> n;
+//     for(int i = 2;i < n;i++)
+//     {
+//         if(n % i == 0)
+//         {
+//             count++;
+//             break;
+//         }
+//     }
+//     if(count == 0)
+//     {
+//         cout << "Prime" << endl;
+//     }
+//     else
+//     {
+//         cout << "Not Prime" << endl;
+//     }
+//     return 0;
+// }
